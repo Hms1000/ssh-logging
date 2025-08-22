198394829085eacb0778b093b977fd86749ce064
Hms1000 <hmstakai@gmail.com>
2025-08-22 08:46:59 +0200

    first script commit

---
+++ src/ssh-logging.py
@@ -0,0 +1,63 @@
+import paramiko
+import argparse
+import logging
+from pathlib import Path
+import os
+
+log_file = Path(__file__).with_name('secure_ssh.log')
+
+# i configure logging because i want the script to generate logs
+logging.basicConfig(
+        filename=log_file,
+        level=logging.INFO,
+        format='%(asctime)s-%(levelname)s-%(message)s'
+        )
+
+def linux_server_secure_logging(hostname, username):
+    ssh_client = paramiko.SSHClient() # ssh client
+    try:
+        ssh_client.add_missing_host_key_policy(paramiko.AutoRejectPolicy()) # adding host key policy, AutoReject=Secure
+        ssh_client.load_system_host_keys() # checking known host list
+        private_key = os.path.expanduser("~/.ssh/id_rsa")  # getting the private key
+
+        # connecting to the server via ssh
+        ssh_client.connect(
+                hostname=hostname,
+                username=username,
+                key_filename=private_key
+                )
+
+        print('Connected securely using existing credentials')
+        logging.info('Connected securely using existing credentials')
+
+        # executing commands after logging in the server
+        stdin,stdout, stderr = ssh_client.exec_command('ls')
+        print('Executing command "ls"')
+        logging.info('Executing command "ls"')
+
+        output = stdout.read().decode()
+        error =  stderr.read().decode()
+        
+        print(f'stdout: {output}')
+        logging.info(f'stdout: {output}')
+
+        print(f'stderr: {error}')
+        logging.error(f'stderr: {error}')
+
+    except Exception as e:
+       print(f'Error: {e}')
+       logging.error(f'Error: {e}')
+
+    finally:
+       ssh_client.close()
+
+def main():
+    parser = argparse.ArgumentParser(description='logging securely into a server using ssh')
+    parser.add_argument('--hostname', type=str, required=True, help='target server ip address')
+    parser.add_argument('--username', type=str, required=True, help='target server username')
+    args = parser.parse_args()
+
+    linux_server_secure_logging(args.hostname, args.username)
+
+if __name__ == '__main__':
+    main()
